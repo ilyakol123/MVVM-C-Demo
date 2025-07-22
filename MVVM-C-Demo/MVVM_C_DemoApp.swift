4ce65aba8c3a13cab1e7bb483fb7ba6f51659a4b
Kolesnikov Ilya <ilja-kolesnikov@mail.ru>
2025-07-22 18:21:28 +0300

    Initial Commit

---
+++ MVVM-C-Demo/MVVM_C_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVM_C_DemoApp.swift
+//  MVVM-C-Demo
+//
+//  Created by Илья Колесников on 22.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct MVVM_C_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
